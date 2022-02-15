c9ec6dd7e12fea7fd5d0670a5959e289dbdb0e2a
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-15 23:31:23 +0530

    Adding environment vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs= require("ejs");
@@ -20,8 +20,7 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-const secret="Thisisourlittlesecret.";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
