59a165139ed67379bfd2b6ec9d115643635772c5
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-17 12:06:38 +0530

    hashing and salting with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const ejs= require("ejs");
 const mongoose=require("mongoose");
 //const encrypt=require("mongoose-encryption");
-const md5=require("md5");
+//const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=5;
+
 
 const app = express();
 
@@ -39,15 +42,17 @@ app.route("/login")
 })
 .post(function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password===password){
+        bcrypt.compare(password,foundUser.password,function(err,result){if (result===true) {
           res.render("secrets");
         }
+
+        });
       }
     }
   });
@@ -62,17 +67,20 @@ app.route("/register")
   res.render("register");
 })
 .post(function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets")
+      }else{
+        console.log(err);
+      }
+    });
   });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets")
-    }else{
-      console.log(err);
-    }
-  });
+
 })
 
 
