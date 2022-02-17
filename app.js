8ba1ca12abffce025b9c42816d69bd10a4fe3296
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-17 15:35:50 +0530

    complete website

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema=new mongoose.Schema({
   email:String,
   password:String,
-  googleId:String
+  googleId:String,
+  secret:String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -171,15 +172,40 @@ app.route("/register")
 });
 
 app.get("/secrets",function(req,res){
+  User.find({"secret":{$ne:null}},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        res.render("secrets",{usersWithSecrets:foundUser});
+      }
+    }
+  });
+});
+
+app.get("/submit",function(req,res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
 });
-
-
-
+app.post("/submit",function(req,res){
+  const submittedSecret=req.body.secret;
+  console.log(req.user.id);
+  User.findById(req.user.id,function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret=submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        })
+      }
+    }
+  })
+});
 
 
 
